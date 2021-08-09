048797efd27eea278d4fed6ccbefa3af3bc1e848
SovaV <sova.volodymyr95@gmail.com>
2021-08-09 16:55:37 +0300

    старт 12 дз

--- src/js/templating.js
+++
@@ -1,9 +0,0 @@
-import menuCardTpl from '../templates/menu-card.hbs';
-import menu from './menu.json';
-
-const menuContainer = document.querySelector('.js-menu');
-const menuMarkup = menuCardTpl(menu);
-
-menuContainer.insertAdjacentHTML('beforeend', menuMarkup);
-
-
